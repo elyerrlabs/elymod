6eed8e88fb7b732ae166aec76708b6cacd36acbd
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-12-28 00:23:19 -0500

    feat: remove custom command
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- stubs/bootstrap/app.php
+++ stubs/bootstrap/app.php
@@ -4,8 +4,5 @@ use Elyerr\LaravelRuntime\App\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withCommands([
-        \{{ Namespace }}\App\Console\Commands\StorageLink::class,
-    ])
     ->withExceptions(function (Exceptions $exceptions) {
     })->create();
