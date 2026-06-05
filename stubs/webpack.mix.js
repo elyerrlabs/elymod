26a37019b8fda07837300abbd59b7a15273a2052
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-06-04 22:41:13 -0500

    chore: updated package.json and lwebpack.mix.js

--- stubs/webpack.mix.js
+++ stubs/webpack.mix.js
@@ -4,8 +4,8 @@ const path = require('path');
 mix.webpackConfig({
     resolve: {
         alias: {
-            "@{{ module }}": path.resolve(__dirname, "resources/js"),
-            "@{{ module }}Css": path.resolve(__dirname, "resources/css")
+            "@": path.resolve(__dirname, "resources/js"),
+            "@css": path.resolve(__dirname, "resources/css")
         },
     },
     stats: {
@@ -15,8 +15,8 @@ mix.webpackConfig({
 })
 
 mix.js('resources/js/app.js', 'js/app.js')
+    .js('resources/js/pages.js', 'js/pages.js')
     .vue({ version: 3 })
     .postCss('resources/css/app.css', 'css/app.css', [
         require('@tailwindcss/postcss'),
-        require("autoprefixer"),
     ]).version();
